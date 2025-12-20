d03acd90d9b58db110c84606cc9972c7ed9b5453
Vlad <Sp0ngbik@gmail.com>
2025-12-20 18:36:47 +0300

    fix: add @types/node

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,24 +1,31 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { setLoadingStatus } from "@/features/api";
 
 export const baseApi = createApi({
   reducerPath: "tmdbApi",
   tagTypes: ["MoviesList", "SearchMovie"],
-  baseQuery: async (args, api, extraOptions) => {
-    // try {
-    api.dispatch(setLoadingStatus(true));
-    const result = await fetchBaseQuery({
-      baseUrl: "https://api.themoviedb.org/3",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-      },
-    })(args, api, extraOptions);
-    // errorHandler(api, result);
-    return result;
-    // } finally {
-    //   api.dispatch(setLoadingStatus(false));
-    // }
-  },
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.themoviedb.org/3",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+    },
+  }),
+  // baseQuery: async (args, api, extraOptions) => {
+  //   try {
+  //     api.dispatch(setLoadingStatus(true));
+  //     const result = await fetchBaseQuery({
+  //       baseUrl: "https://api.themoviedb.org/3",
+  //       headers: {
+  //         accept: "application/json",
+  //         Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+  //       },
+  //     })(args, api, extraOptions);
+  //     errorHandler(api, result);
+  //     return result;
+  //   } finally {
+  //     api.dispatch(setLoadingStatus(false));
+  //   }
+  // },
+
   endpoints: () => ({}),
 });
