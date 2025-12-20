a99bd6c5327df212b7f180a1827c6859245d6e04
Vlad <Sp0ngbik@gmail.com>
2025-12-20 18:28:40 +0300

    fix: add @types/node

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -6,20 +6,20 @@ export const baseApi = createApi({
   reducerPath: "tmdbApi",
   tagTypes: ["MoviesList", "SearchMovie"],
   baseQuery: async (args, api, extraOptions) => {
-    try {
-      api.dispatch(setLoadingStatus(true));
-      const result = await fetchBaseQuery({
-        baseUrl: "https://api.themoviedb.org/3",
-        headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-        },
-      })(args, api, extraOptions);
-      errorHandler(api, result);
-      return result;
-    } finally {
-      api.dispatch(setLoadingStatus(false));
-    }
+    // try {
+    api.dispatch(setLoadingStatus(true));
+    const result = await fetchBaseQuery({
+      baseUrl: "https://api.themoviedb.org/3",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+      },
+    })(args, api, extraOptions);
+    // errorHandler(api, result);
+    return result;
+    // } finally {
+    //   api.dispatch(setLoadingStatus(false));
+    // }
   },
   endpoints: () => ({}),
 });
