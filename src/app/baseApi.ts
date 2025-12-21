b311ef213eda86c295b0f8462ceca6a33cc50acd
Vlad <Sp0ngbik@gmail.com>
2025-12-21 14:37:57 +0300

    fix: add @types/node

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,11 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { setLoadingStatus } from "@/features/api";
+import { errorHandler } from "@/common/variables";
 
 export const baseApi = createApi({
   reducerPath: "tmdbApi",
   tagTypes: ["MoviesList", "SearchMovie"],
   baseQuery: async (args, api, extraOptions) => {
-    api.dispatch(setLoadingStatus(true));
+    // api.dispatch(setLoadingStatus(true));
     const result = await fetchBaseQuery({
       baseUrl: "https://api.themoviedb.org/3",
       headers: {
@@ -13,8 +13,8 @@ export const baseApi = createApi({
         Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
       },
     })(args, api, extraOptions);
-    // errorHandler(api, result);
-    api.dispatch(setLoadingStatus(false));
+    errorHandler(api, result);
+    // api.dispatch(setLoadingStatus(false));
     return result;
   },
 
